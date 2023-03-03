26f58ced74207d38d02c8a3ccf4adbe347d3d23f
Kwan Ting Lau <kestino@iMac.local>
2023-03-03 05:09:18 -0500

    Finished main page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,27 @@
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BgSquare from "./BgSquare";
 import Profile from "../assets/PfP3.0.png";
+import { useRef, useState } from "react";
 
 function Main() {
+  // const personalTraits: string[] = ["const [hobby, setHobby] = useState('Pool');"];
   const personalTraits: string[] = ["Hi from Kestino!", "AvidSelfLearner.py",
                                     "const [hobby, setHobby] = useState('Pool');", "<LoveWebDevMoreTho />"];
-  // const personalTraits: string[] = ["const [hobby, setHobby] = useState('Pool');"];
   const [text, count] = useTypewriter({words: personalTraits, loop: true, delaySpeed: 2000,});
+
+  const about = useRef<HTMLAnchorElement>(null), exp = useRef<HTMLAnchorElement>(null);
+  const skills = useRef<HTMLAnchorElement>(null), projs = useRef<HTMLAnchorElement>(null);
+  const adjust = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, upper: boolean) => {
+    const winW = window.innerWidth, denom = about.current?.offsetWidth; let perc = 68;
+    const left: boolean = e.clientX < winW / 2;
+    (left) ? perc += 30 * (e.clientX / denom!) : perc += 30 * ((winW - e.clientX) / denom!);
+    (upper ? (left ? about : skills) : (left ? exp : projs)).current?.style.setProperty("--trans", `${perc}%`);
+  };
   return (
     <div className="fjbic">
       <div className="linksize z-30 fjc fc hs border-white">
-        <a href="#about" className="link llink w-full fjsic z-30 border-r-2">about</a>
-        <a href="#experience" className="link llink w-full fjsic z-30 border-b-2 border-r-2">experience</a>
+        <a href="#about" ref={about} onMouseMove={e => adjust(e, true)} className="link llink w-full fjsic z-30 border-r-2">about</a>
+        <a href="#experience" ref={exp} onMouseMove={e => adjust(e, false)} className="link llink w-full fjsic z-30 border-b-2 border-r-2">experience</a>
       </div>
       <div className="fjcic fc hs space-y-8 tc">
           <BgSquare />
@@ -25,8 +35,8 @@ function Main() {
           </div>
       </div>
       <div className="linksize z-30 fjc fc hs pr-4 border-white">
-        <a href="#skills" className="link rlink w-full fjeic z-30 border-l-2">skills</a>
-        <a href="#projects" className="link rlink w-full fjeic z-30 border-b-2 border-l-2">projects</a>
+        <a href="#skills" ref={skills} onMouseMove={e => adjust(e, true)} className="link rlink w-full fjeic z-30 border-l-2">skills</a>
+        <a href="#projects" ref={projs} onMouseMove={e => adjust(e, false)} className="link rlink w-full fjeic z-30 border-b-2 border-l-2">projects</a>
       </div>
     </div>
     
