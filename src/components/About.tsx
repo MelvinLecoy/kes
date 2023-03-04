04f2344845260515862bd6c90f4d1086a4815ac1
Kwan Ting Lau <kestino@iMac.local>
2023-03-04 05:30:43 -0500

    Added hackereffect

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,35 @@
 import { motion } from "framer-motion";
-import AboutPic from "../assets/images/About/About.png";
 import SpreadEffect from "./SpreadEffect";
 import aboutList from "../assets/data/aboutlist.json"
 import CardDeck from "./CardDeck";
 
+type event = React.MouseEvent<HTMLHeadingElement, MouseEvent> | React.MouseEvent<HTMLLIElement, MouseEvent>;
+
 export default function About() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const hacker = (e: event) => {
+    let itr = 0;
+    const interval = setInterval(() => {
+      const et = e.target as HTMLElement;
+      et.innerText = et.innerText.split("").map((l, idx) => {
+          return (idx < itr) ? et.dataset.value![idx] : letters[Math.floor(Math.random() * 52)];
+      }).join("");
+      if (itr >= et.dataset.value!.length) clearInterval(interval);
+      itr += 1/3;
+    }, 30);
+  };
+
   return (
   <motion.div initial={{opacity: 0,}} whileInView={{opacity: 1,}} transition={{duration: 1.5}}
               className='pageDiv z-20'>
     <SpreadEffect title="About" />
     <div className="fjbic fc ws 2xl:fr 2xl:fjevenic h-[83%]">
       <CardDeck />
-      <div className="2xl:ml-60">
-        <h4 className="font-semibold underline mb-2 text-[40px] 2xl:text-[50px]">Kestino (He/Him)</h4>
-        <ul className="list-disc tl ml-4 space-y-[6px] text-[20px] xl:text-[21px] 2xl:text-[24px]
+      <div className="2xl:ml-60 w-[450px] 2xl:w-[500px]">
+        <h4 onMouseOver={hacker} className="font-semibold underline mb-2 text-[40px] 2xl:text-[50px]" data-value="Kestino (He/Him)">Kestino (He/Him)</h4>
+        <ul className="list-disc tl ml-[68px] space-y-[6px] text-[20px] xl:text-[21px] 2xl:text-[24px]
                                                       pb-40 sm:pb-20 min-[830px]:pb-16 xl:pb-14 2xl:pb-48">
-          {aboutList.map((list, idx) => <li key={idx}>{list}</li>)}
+          {aboutList.map((list, idx) => <li key={idx} onMouseOver={hacker} data-value={list}>{list}</li>)}
         </ul>
       </div>
     </div>
